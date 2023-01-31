0724120c4fcc5739d465791b8dae2282be0ef21d
youyongfuLs <youyongfuLS@jomoo.cn>
2023-01-31 10:47:06 +0800

    创建项目

---
+++ one-piece-boot/src/main/java/com/you/OnePieceAppletBootApplication.java
@@ -0,0 +1,15 @@
+package com.you;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePieceAppletBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnePieceAppletBootApplication.class, args);
+    }
+    
+    
+}
