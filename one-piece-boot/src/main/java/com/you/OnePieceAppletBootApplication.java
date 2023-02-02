ab5b7a17bea6204ef0866e45cd4818e713f2082c
youyongfuLs <youyongfuLS@jomoo.cn>
2023-02-02 11:01:56 +0800

    整合Mybatis plus框架

--- one-piece-boot/src/main/java/com/you/OnePieceAppletBootApplication.java
+++ one-piece-boot/src/main/java/com/you/OnePieceAppletBootApplication.java
@@ -1,9 +1,10 @@
 package com.you;
 
-
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.you.mapper")   // 扫描mapper接口包，不用在接口上加@Mapper注解
 @SpringBootApplication
 public class OnePieceAppletBootApplication {
 
