a08943f2bb8f357e0afe3dce58c3b584285163d6
chenfuyuan <chenfuyuan@xihuanwu.com>
2023-02-08 22:36:16 +0800

    feat: 修复加载不到 数据库信息问题

---
+++ one-piece-boot/src/main/java/com/you/config/DruidDataSourceWrapper.java
@@ -0,0 +1,15 @@
+package com.you.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * DruidDataSourceWrapper
+ *
+ * @author chenfuyuan
+ * @date 2023/2/8 22:34
+ */
+@ConfigurationProperties("spring.datasource.druid")
+public class DruidDataSourceWrapper extends DruidDataSource {
+
+}
