e59456d2a7d1021a93773f75972025a9f00fc7c8
youyongfuLs <youyongfuLS@jomoo.cn>
2023-03-21 15:36:49 +0800

    岛屿管理-代码重构

--- one-piece-boot/src/main/java/com/you/service/impl/SysIslandsServiceImpl.java
+++ one-piece-boot/src/main/java/com/you/service/impl/SysIslandsServiceImpl.java
@@ -11,14 +11,9 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.you.common.ResultBean;
 import com.you.constant.IslandsConstant;
 import com.you.constant.OssConstant;
-import com.you.entity.SysClobContent;
-import com.you.entity.SysIslands;
-import com.you.entity.SysUploadFile;
+import com.you.entity.*;
 import com.you.mapper.SysIslandsMapper;
-import com.you.service.SysClobContentService;
-import com.you.service.SysIslandsService;
-import com.you.service.SysUploadFileRecordService;
-import com.you.service.SysUploadFileService;
+import com.you.service.*;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -41,6 +36,10 @@ public class SysIslandsServiceImpl extends ServiceImpl<SysIslandsMapper, SysIsla
     private SysUploadFileService sysUploadFileService;
     @Resource
     private SysUploadFileRecordService sysUploadFileRecordService;
+    @Resource
+    private SysOrganizationService sysOrganizationService;
+    @Resource
+    private SysFigureIslandsService sysFigureIslandsService;
 
     /**
      * 分页获取列表
@@ -116,7 +115,7 @@ public class SysIslandsServiceImpl extends ServiceImpl<SysIslandsMapper, SysIsla
         //保存岛屿信息
         String islandsId = UUID.randomUUID().toString().replaceAll("-","");
         sysIslands.setId(islandsId);
-        //未选择上级菜单，则默认为添加目录
+        //未选择上级菜单，则默认为添加一级岛屿
         if(StringUtils.isBlank(sysIslands.getParentId())){
             sysIslands.setParentId("0");
         }
@@ -124,11 +123,7 @@ public class SysIslandsServiceImpl extends ServiceImpl<SysIslandsMapper, SysIsla
         save(sysIslands);
 
         //保存岛屿内容信息
-        List<SysClobContent> contentList = new ArrayList<>();
-        contentList.add(sysClobContentService.assemblyData(islandsId,sysIslands.getSource(), IslandsConstant.SOURCE_TYPE));
-        contentList.add(sysClobContentService.assemblyData(islandsId,sysIslands.getGeography(), IslandsConstant.GEOGRAPHY_TYPE));
-        contentList.add(sysClobContentService.assemblyData(islandsId,sysIslands.getAppearances(), IslandsConstant.APPEARANCES_TYPE));
-        sysClobContentService.saveBatch(contentList);
+        saveOrUpdateContent(sysIslands,"save");
 
         return ResultBean.success(sysIslands);
     }
@@ -142,7 +137,7 @@ public class SysIslandsServiceImpl extends ServiceImpl<SysIslandsMapper, SysIsla
     public ResultBean getInfoById(String id) {
         MapBuilder<Object, Object> map = MapUtil.builder();
 
-        //获取组织信息
+        //获取岛屿信息
         SysIslands sysIslands = getById(id);
         map.put("islands",sysIslands);
 
@@ -150,7 +145,7 @@ public class SysIslandsServiceImpl extends ServiceImpl<SysIslandsMapper, SysIsla
         List<SysUploadFile> fileList = sysUploadFileService.getFileInfo(OssConstant.ISLANDS_TYPE,sysIslands.getId());
         map.put("fileList",fileList);
 
-        //获取组织内容信息
+        //获取岛屿内容信息
         List<SysClobContent> islandsContentList = sysClobContentService.contentListByOwnerId(id);
         islandsContentList.forEach(conten ->{
             map.put(conten.getType(),conten.getContent());
@@ -167,7 +162,6 @@ public class SysIslandsServiceImpl extends ServiceImpl<SysIslandsMapper, SysIsla
     @Override
     public ResultBean updateIslands(SysIslands sysIslands) {
         //更新操作
-        String islandsId = sysIslands.getId();
         if(StringUtils.isBlank(sysIslands.getParentId())){
             sysIslands.setParentId("0");
         }
@@ -175,20 +169,9 @@ public class SysIslandsServiceImpl extends ServiceImpl<SysIslandsMapper, SysIsla
         updateById(sysIslands);
 
         //更新岛屿内容信息
-        List<SysClobContent> islandsContentList = sysClobContentService.contentListByOwnerId(islandsId);
-        islandsContentList.forEach(content ->{
-            if(IslandsConstant.SOURCE_TYPE.equals(content.getType())){
-                content.setContent(sysIslands.getSource());
-            }else if(IslandsConstant.GEOGRAPHY_TYPE.equals(content.getType())){
-                content.setContent(sysIslands.getGeography());
-            }else if(IslandsConstant.APPEARANCES_TYPE.equals(content.getType())){
-                content.setContent(sysIslands.getAppearances());
-            }
-        });
-        sysClobContentService.updateBatchById(islandsContentList);
+        saveOrUpdateContent(sysIslands,"update");
 
-
-        //删除已保存的组织文件关系
+        //删除已保存的岛屿文件关系
         if(StringUtils.isNotBlank(sysIslands.getFileIds())){
             List<String> fileIds = Arrays.asList(sysIslands.getFileIds().split(","));
             sysUploadFileRecordService.deleteFileRecord(OssConstant.ISLANDS_TYPE,fileIds);
@@ -204,10 +187,20 @@ public class SysIslandsServiceImpl extends ServiceImpl<SysIslandsMapper, SysIsla
      */
     @Override
     public ResultBean deleteIslands(String id) {
-        //判断该菜单是否存在子菜单，存在无法删除
+        //判断该菜单是否存在子岛屿，存在无法删除
         int count = count(new QueryWrapper<SysIslands>().eq("parent_id", id));
         if (count > 0) {
-            return ResultBean.fail("请先删除子岛屿");
+            return ResultBean.fail("该岛屿存在下属岛屿，无法删除！");
+        }
+
+        count = sysOrganizationService.count(new QueryWrapper<SysOrganization>().eq("headquarters", id));
+        if (count > 0) {
+            return ResultBean.fail("该岛屿被组织引用，无法删除！");
+        }
+
+        count = sysFigureIslandsService.count(new QueryWrapper<SysFigureIslands>().eq("islands_id", id));
+        if (count > 0) {
+            return ResultBean.fail("该岛屿被人物引用，无法删除！");
         }
 
         //删除岛屿
@@ -219,4 +212,33 @@ public class SysIslandsServiceImpl extends ServiceImpl<SysIslandsMapper, SysIsla
         return ResultBean.success();
     }
 
+    /**
+     * 保存/更新岛屿内容信息
+     * @param sysIslands
+     * @param type
+     */
+    private void saveOrUpdateContent(SysIslands sysIslands, String type) {
+        String islandsId = sysIslands.getId();
+        if("save".equals(type)){
+            //保存
+            List<SysClobContent> contentList = new ArrayList<>();
+            contentList.add(sysClobContentService.assemblyData(islandsId,sysIslands.getSource(), IslandsConstant.SOURCE_TYPE));
+            contentList.add(sysClobContentService.assemblyData(islandsId,sysIslands.getGeography(), IslandsConstant.GEOGRAPHY_TYPE));
+            contentList.add(sysClobContentService.assemblyData(islandsId,sysIslands.getAppearances(), IslandsConstant.APPEARANCES_TYPE));
+            sysClobContentService.saveBatch(contentList);
+        }else {
+            //更新
+            List<SysClobContent> islandsContentList = sysClobContentService.contentListByOwnerId(islandsId);
+            islandsContentList.forEach(content ->{
+                if(IslandsConstant.SOURCE_TYPE.equals(content.getType())){
+                    content.setContent(sysIslands.getSource());
+                }else if(IslandsConstant.GEOGRAPHY_TYPE.equals(content.getType())){
+                    content.setContent(sysIslands.getGeography());
+                }else if(IslandsConstant.APPEARANCES_TYPE.equals(content.getType())){
+                    content.setContent(sysIslands.getAppearances());
+                }
+            });
+            sysClobContentService.updateBatchById(islandsContentList);
+        }
+    }
 }
