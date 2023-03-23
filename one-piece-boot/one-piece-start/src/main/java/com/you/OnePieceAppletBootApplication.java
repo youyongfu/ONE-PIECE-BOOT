dcb7581541a39ebbe705e9dec7040fabe11d8ba6
youyongfuLs <youyongfuLS@jomoo.cn>
2023-03-23 16:33:44 +0800

    模块拆分

--- one-piece-boot/one-piece-start/src/main/java/com/you/OnePieceAppletBootApplication.java
+++ one-piece-boot/one-piece-start/src/main/java/com/you/OnePieceAppletBootApplication.java
@@ -2,8 +2,10 @@ package com.you;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.you.*"})  //指定扫描包路径
 public class OnePieceAppletBootApplication {
 
     public static void main(String[] args) {
